2244f79031e61fd829e10bca1f7a30e8eef34703
OyamaErika <oyama.erika@tis.co.jp>
2018-06-11 22:06:27 +0900

    作成

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,28 @@
 package codecheck;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+			String strNum = String.format("argv[%s]: %s", i, args[i]);
+			Pattern p = Pattern.compile("[0-10001]");
+			Matcher m = p.matcher(strNum);
+			int num = Integer.valueOf(strNum).intValue();
+			if (m.find()) {
+				if (num % 3 == 0 && Integer.toString(num).contains("3")) {
+					System.out.println("dumb");
+				} else if (num % 3 == 0) {
+					System.out.println("idiot");
+				}else if (Integer.toString(num).contains("3")) {
+					System.out.println("stupid");
+				} else {
+					System.out.println("smart");
+				}
+			} else {
+				System.out.println("invalid");
+			}
 		}
 	}
 }
